98173e1ab09dff7bcc5896b31f83d7a6266c6c42
Shqipe Kamberi <shqipe.kamberi2004@gmail.com>
2026-01-30 23:53:11 +0100

    Created database tables for characters, locations, events

---
+++ database/migrations/2026_01_30_224718_create_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('type')->nullable();
+            $table->string('climate')->nullable();
+            $table->integer('population')->nullable();
+            $table->text('notable_features')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
